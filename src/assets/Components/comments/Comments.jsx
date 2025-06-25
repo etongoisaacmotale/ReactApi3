2a54e959a7fb9f04fb8fb9868c4ebf73876a80ed
Sina Moganedi <srmoganedi@gmail.com>
2025-06-25 12:14:44 +0200

    made some changes to comments file, shows a loading message at first,fetches comments data from and api using axios.get, display the data once they are received, show error message if the request fail

--- src/assets/Components/comments/Comments.jsx
+++ src/assets/Components/comments/Comments.jsx
@@ -1,9 +1,54 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class Comments extends Component {
+    constructor() {
+      super()
+    
+      this.state = {
+         comments:[],
+         loading: true,
+         error:null
+      }
+    }
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts/1/comments")
+        .then(response=>{
+            this.setState({
+               comments:response.data,
+               loading:false 
+            })
+        }).catch(error =>{
+            this.setState({
+                error: error.message,
+                loading:false
+            })
+        })
+    }
   render() {
+    const {comments,error,loading}=this.state
+    if(loading){
+        return <div>loading.......</div>
+    }
+    if(error){
+        return <div>Error: {error}</div>
+    }
     return (
-      <div>Comments</div>
+      <div>
+        <h1>Comments</h1>
+        <ul>
+            {comments.map(comment =>(
+                <li key = {comment.id}>
+                   <strong> {comment.name}</strong>
+                   <p>{comment.email}</p>
+                   <p>{comment.body}</p>
+
+                </li>
+                
+            ))}
+            
+        </ul>
+      </div>
     )
   }
 }
