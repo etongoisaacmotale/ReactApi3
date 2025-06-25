d44322f3134dea2c747d402025e844d4c2033fa0
etongoisaacmotale <etongoisaacmotale@gmail.com>
2025-06-25 19:05:42 +0200

    added album

--- src/assets/Components/album/Album.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Album extends Component {
-  render() {
-    return (
-      <div>Album</div>
-    )
-  }
-}
--- src/assets/Components/photos/Photos.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Photos extends Component {
-  render() {
-    return (
-      <div>Photos</div>
-    )
-  }
-}
