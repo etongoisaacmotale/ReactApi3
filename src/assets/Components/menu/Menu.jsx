30f70ceae073bf9a0b8e8098c2022e670cfb57d3
Sina Moganedi <srmoganedi@gmail.com>
2025-06-25 18:05:48 +0200

    trying to pull

--- src/assets/Components/menu/Menu.jsx
+++ src/assets/Components/menu/Menu.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom';
 export default class Menu extends Component {
   render() {
     return (
-      <nav style={{ padding: '1rem', background: '#eee', marginBottom: '2rem' }}>
-        <Link to="/posts" style={{ marginRight: '1rem' }}>Posts</Link>
-        <Link to="/comments" style={{ marginRight: '1rem' }}>Comments</Link>
-        <Link to="/albums" style={{ marginRight: '1rem' }}>Albums</Link>
-        <Link to="/photos" style={{ marginRight: '1rem' }}>Photos</Link>
-        <Link to="/todos">Todos</Link>
-      </nav>
+     <div>
+      <p>The available nested routes are:</p>   
+ <ul className="nav-list">
+  <li><Link to="/Comments" className="btn btn-link">Comments</Link></li>
+  <li><Link to="/Posts" className="btn btn-link">Posts</Link></li>
+  <li><Link to="/Photos" className="btn btn-link">Photos</Link></li>
+  <li><Link to="/Album" className="btn btn-link">Albums</Link></li>
+</ul>
+  
+     </div>
     );
   }
 }
