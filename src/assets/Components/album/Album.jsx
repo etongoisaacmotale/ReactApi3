17922a8cf93c1315bcf15ce43ba33e9d2607d4a1
etongoisaacmotale <etongoisaacmotale@gmail.com>
2025-06-25 20:56:45 +0200

    commit by isaac

---
+++ src/assets/Components/album/Album.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+
+export default class Album extends Component {
+    constructor(props) {
+      super()
+    
+      this.state = {
+         album: [],
+         loading: true,
+         error: null
+      }
+    }
+    
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/albums")
+        .then(response=>{
+            this.setState({
+               album:response.data,
+               loading:false 
+            })
+        }).catch(error =>{
+            this.setState({
+                error: error.message,
+                loading:false
+            })
+        })
+    }
+    
+
+    render() {
+        const {album,error,loading}=this.state
+        if(loading){
+            return <div>loading.......</div>
+        }
+        if(error){
+            return <div>Error: {error}</div>
+        }
+        return (
+          <div>
+            <h1>Albums</h1>
+            <ul>
+                {album.map(album =>(
+                    <li key = {album.id}>
+                       <strong> {album.name}</strong>
+                       <p>{album.email}</p>
+                       <p>{album.body}</p>
+    
+                    </li>
+                    
+                ))}
+                
+            </ul>
+          </div>
+        )
+      }
+}
